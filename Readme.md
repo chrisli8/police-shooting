1bf03e21ff1a1f978312ab4974577738a16ad73f
Chris Li <8chrisgli@gmail.com>
2015-10-21 01:31:31 -0700

    new changes

--- Readme.md
+++ Readme.md
@@ -1,3 +1,5 @@
 # Police shootings
 
 This repository is a challenge for the following [course](http://faculty.washington.edu/mikefree/info343/) at the University of Washington.  The instructions are described [here](http://faculty.washington.edu/mikefree/info343/#/challenges/map-challenge).
+
+I've implemented all of the features listed in the instuctions. This assignment took me quite a while to finishes, around 10 hours.
--- js/map.js
+++ js/map.js
@@ -1,35 +1,95 @@
-// Function to draw your map
+//Christopher Li, INFO 343 C, 10/21/15
+
+// Function to draw your map initially empty
 var drawMap = function() {
-
-  // Create map and set view
- 
-
-  // Create a tile layer variable using the appropriate url
-
-
-  // Add the layer to your map
- 
-
-  // Execute your function to get data
- 
+	var map = L.map('container').setView([38, -92], 4);
+	var layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
+ 	layer.addTo(map);
+ 	getData(map);
 }
 
-// Function for getting data
-var getData = function() {
-
-  // Execute an AJAX request to get the data in data/response.js
-
-
-  // When your request is successful, call your customBuild function
+// Sends an asycronous ajax request to get data about police shootings
+var getData = function(map) {
+	$.ajax({
+	    url:'data/response.json',
+	    type: "get",
+	    success:function(dat) {
+	      customBuild(dat,map)
+	    }, 
+	   dataType:"json"
+	}) 
 
 }
 
-// Loop through your data and add the appropriate layers and points
-var customBuild = function() {
-	// Be sure to add each layer to the map
+// Loops through data and adds the appropriate layers and points and table
+var customBuild = function(data, map) {
+	var maleK = 0;
+	var maleH = 0;
+	var femK = 0;
+	var femH = 0;
 
-	// Once layers are on the map, add a leaflet controller that shows/hides layers
-  
+	var jsonArry = data;
+	var layersObj = new Object();
+	//loops through all objects and processes their data
+	for(var i = 0; i < jsonArry.length; i++) {
+		var race = "\"Unknown\"";
+		if("Race" in jsonArry[i]) {
+			race = JSON.stringify(jsonArry[i].Race);
+		}
+		if(!layersObj.hasOwnProperty(race)) {
+			layersObj[race] = new L.LayerGroup([]);
+		}
+		//checks if latitude and logitude are defined
+		if(("lat" in jsonArry[i]) && ("lng" in jsonArry[i])) {
+			//pink = european american; brown = african american; yellow = asian american
+			var clr = '#6B6B6B';
+			if(race == '\"White\"') {
+				clr = '#FFCCCC';
+			} else if (race == '\"Black or African American\"') {
+				clr = '#331F14';
+			} else if (race == '\"Asian\"') {
+			 	clr = '#FFFFCC';
+			}
+
+			//red borders signify female victim
+			var genC = '#990033';
+			if(jsonArry[i]['Victim\'s Gender'] == 'Male') {
+				genC = clr;
+			}
+			var circle = new L.circleMarker([jsonArry[i].lat, jsonArry[i].lng], {color:genC, weight:'2', fillColor:clr, opacity:0.7, fillOpacity: 0.7}).setRadius(4);
+			circle.bindPopup(jsonArry[i].Summary);
+			circle.addTo(layersObj[race]);
+		}
+
+		//catalogs the data of male or female; killed or hit
+		if(jsonArry[i]['Victim\'s Gender'] == 'Male') {
+			if(jsonArry[i]['Hit or Killed?'] == 'Killed') {
+				maleK ++;
+			} else {
+				maleH ++;
+			}
+		} else {
+			if(jsonArry[i]['Hit or Killed?'] == 'Killed') {
+				femK ++;
+			} else {
+				femH ++;
+			}
+		}
+	}
+
+	//puts layers on map and adds controls
+	for(var key in layersObj) {
+		layersObj[key].addTo(map);
+	}
+	L.control.layers(null,layersObj).addTo(map);
+
+	//add a table above map about data cataloged
+	var table = "<table><tr><th></th><th>Killed</th><th>Hit</th><th>Percent Killed</th>" + 
+				"</tr><tr><th>Male</th><td>" + maleK + "</td><td>" + maleH + "</td><td>" + 
+				Math.round(maleK * 100 / (maleK + maleH)) + "%" + "</td></tr><tr><th>Female</th><td>" + 
+				femK + "</td><td>" + femH + "</td><td>" + 
+				Math.round(femK * 100/ (femK + femH)) + "%" + "</td></tr></table>";
+	$("#table").append(table);
 }
 
 
